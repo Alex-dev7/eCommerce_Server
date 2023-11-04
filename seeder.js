8c8da23dea398164888461b2108572015e25a1bd
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-04 14:34:39 -0400

    seed the data in DB

--- seeder.js
+++ seeder.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import colors from "colors";
-import users from "./data/users";
-import products from "./data/products"
-import User from "./models/userModel";
-import Product from "./models/productModel";
-import Order from "./models/orderModel";
+import users from "./data/users.js";
+import products from "./data/products.js"
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
 import connectDB from "./config/db.js"
 
 dotenv.config()
@@ -51,4 +51,11 @@ const destroyData = async () => {
         console.error(`${error}`.red.inverse)
         process.exit(1)
     }
+}
+
+
+if(process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
 }
