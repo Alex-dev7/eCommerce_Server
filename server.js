3e9c1eea96a4e110264a4c6b3fe99f5d15abe02a
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-08 21:09:37 -0500

    user email and password validation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ dotenv.config()
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productsRoutes from './routes/productsRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 
 connectDB()
 
@@ -11,11 +13,16 @@ const port = process.env.PORT || 8000
 
 const app = express()
 
+//  Body parser middleware 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send("API is running...")
 })
 
 app.use('/api/products', productsRoutes)
+app.use('/api/users',userRoutes)
 
 // middlewares
 app.use(notFound)
