86c8e22e192b088aeb26ca121bb4a9c22bb4623a
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-14 20:58:14 -0500

    order routes and controller

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productsRoutes from './routes/productsRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 
 connectDB()
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productsRoutes)
 app.use('/api/users',userRoutes)
+app.use('/api/orders', orderRoutes)
 
 // middlewares
 app.use(notFound)
